3ec281bfa50466e9114a8ea9f3b19a78f9892c51
github username <github email address>
2020-06-02 13:09:52 -0400

    added null false

--- db/migrate/20200602170347_create_songs.rb
+++ db/migrate/20200602170347_create_songs.rb
@@ -4,7 +4,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :mp3
       t.string :cover_art
-      t.belongs_to :artist, foreign_key: true
+      t.belongs_to :artist, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200602170737_create_playlist_songs.rb
+++ db/migrate/20200602170737_create_playlist_songs.rb
@@ -1,8 +1,8 @@
 class CreatePlaylistSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :playlist_songs do |t|
-      t.belongs_to :playlist, foreign_key: true
-      t.belongs_to :song, foreign_key: true
+      t.belongs_to :playlist, null: false, foreign_key: true
+      t.belongs_to :song, null: false, foreign_key: true
 
       t.timestamps
     end
