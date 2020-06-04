b27a16028db5c65a76c437d91bfb317b45893fb9
github username <github email address>
2020-06-04 17:49:36 -0400

    serialized

--- app/serializers/playlist_song_serializer.rb
+++ app/serializers/playlist_song_serializer.rb
@@ -1,5 +1,13 @@
 class PlaylistSongSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :playlist
-  has_one :song
+  attributes :id, :playlist_id, :song
+  
+  def song
+    {
+      id: self.object.song.id,
+      name: self.object.song.name,
+      mp3: self.object.song.mp3,
+      cover_art: self.object.song.cover_art,
+      artist: self.object.song.artist
+    }
+  end
 end
