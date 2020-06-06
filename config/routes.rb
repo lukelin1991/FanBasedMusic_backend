7a0af614e00db8d0edbb38c81c56acec2634d977
github username <github email address>
2020-06-06 18:54:34 -0400

    routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :playlist_songs
-  resources :playlists
-  resources :songs
+  resources :playlist_songs, only: [:index, :show, :create, :update, :destroy]
+  resources :playlists, only: [:index, :create, :show, :update, :destroy]
+  resources :songs, only: [:index]
   resources :artists
-  resources :users
+  resources :users, only: [:index, :show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
