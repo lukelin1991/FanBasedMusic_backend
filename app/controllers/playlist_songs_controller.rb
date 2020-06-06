fde1b53c25f96921644f7c9278ecd26364f3f23b
github username <github email address>
2020-06-06 18:52:14 -0400

    controllers done

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -1,2 +1,31 @@
 class PlaylistSongsController < ApplicationController
+
+    def index
+        @playlist_songs = PlaylistSong.all
+        @users = User.all
+        @playlists = Playlist.all
+        @songs = Song.all
+        @artist = Artist.all
+    end
+
+    def show
+        @playlist_song = PlaylistSong.find(params[:id])
+        render json: @playlist_song.to_json(except: [:created_at, :updated_at])
+    end
+
+    def create
+        playlist_song = PlaylistSong.create(playlist_song_params)
+        render json: playlist_song
+    end
+
+    def destroy
+        @playlist_song = PlaylistSong.find(params[:id])
+        @playlist_song.destroy
+    end
+
+    private
+
+    def playlist_song_params
+        params.permit(:playlist_id, :song_id)
+    end
 end
--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,35 @@
 class PlaylistsController < ApplicationController
+    before_action :set_playlist, only: [:show, :update, :destroy]
+
+    def index
+        @playlists = Playlist.all
+        render json: @playlists.to_json(except: [:created_at, :updated_at])
+    end
+
+    def show
+        render json: @playlist.to_json(except: [:created_at, :updated_at])
+    end
+
+    def create
+        playlist = Playlist.create(playlist_params)
+    end
+
+    def update
+        @playlist.update(playlist_params)
+        render json: @playlist.to_json(except: [:created_at, :updated_at])
+    end
+
+    def destroy
+        @playlist.destroy
+    end
+
+    private
+
+    def set_playlist
+        @playlist = Playlist.find(params[:id])
+    end
+
+    def playlist_params
+        params.permit(:name, :user_id)
+    end
 end
