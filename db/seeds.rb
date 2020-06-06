9721aa73d4dd677f4ebb8dd230899c73ed92acea
github username <github email address>
2020-06-06 17:51:23 -0400

    creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PlaylistSong.destroy_all
+Song.destroy_all
+Playlist.destroy_all
+Artist.destroy_all
+User.destroy_all
+
